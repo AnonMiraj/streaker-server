e52c82dd7dcafb277857700ddbdbb0a4a3bf9f57
AnonMiraj <nabilmalek48@gmail.com>
2024-02-16 06:09:06 +0000

    improve the data base

--- api/models.py
+++ api/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Trainees(models.Model):
     discord_id = models.TextField(primary_key=True, max_length=30)
     discord_pfp = models.TextField(max_length=150)
     discord_name = models.TextField(max_length=30)
-    total_days = models.IntegerField(default=0)
-    total_problems = models.IntegerField(default=0)
-    highest_streak = models.IntegerField(default=0)
-    current_streak = models.IntegerField(default=0)
+    last_activity = models.DateTimeField(default=timezone.now)
+
+    total_days = models.PositiveIntegerField(default=0)
+    total_problems = models.PositiveIntegerField(default=0)
+    highest_streak = models.PositiveIntegerField(default=0)
+    current_streak = models.PositiveIntegerField(default=0)
 
     class Meta:
         db_table = 'trainees'
@@ -24,10 +27,10 @@ class Trainees(models.Model):
 
 class TraineeRecords(models.Model):
     discord_id = models.TextField(max_length=30)
-    post_date = models.DateTimeField()
+    post_date = models.DateTimeField(default=timezone.now)
     message = models.TextField(max_length=150)
-    streak = models.IntegerField(default=0)
-    today_problems = models.IntegerField(default=0)
+    streak = models.PositiveIntegerField(default=0)
+    today_problems = models.PositiveIntegerField(default=0)
 
     trainee = models.ForeignKey(Trainees, on_delete=models.CASCADE, related_name='trainee_records')
 
@@ -41,25 +44,14 @@ class TraineeRecords(models.Model):
             self.trainee.update_streaks(new_streak)
             self.trainee.total_days += 1
             self.trainee.total_problems += self.today_problems
+            self.trainee.last_activity = self.post_date
             self.trainee.save()
         else:
             old_record = TraineeRecords.objects.get(pk=self.pk)
-            old_problems = old_record.today_problems
+            self.trainee.total_problems += self.today_problems - old_record.today_problems
 
-            self.trainee.total_problems -= old_problems
-            self.trainee.total_problems += self.today_problems
-            self.trainee.save()
-            #
-            # subsequent_records = self.trainee.trainee_records.filter(post_date__gt=self.post_date).order_by('post_date')
-            # current_streak = self.streak
-            #
-            # for record in subsequent_records:
-            #     if record.streak == current_streak+1:
-            #         record.streak -= 1
-            #     else:
-            #         break
-            #     current_streak += 1
-            #     record.save()
+        self.trainee.save()
+
         super().save(*args, **kwargs)
 
     def __str__(self):
