d7dab236371d1aafe022c4a2070d3870c6a87f2c
AnonMiraj <nabilmalek48@gmail.com>
2024-02-20 14:10:29 +0000

    Revert "test"
    This reverts commit aea6646c7e59e0a149876a6e5ffa68114b34a418.

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Trainee, TraineeRecord
 
+
 @admin.register(Trainee)
 class TraineeAdmin(admin.ModelAdmin):
     list_display = ('discord_id', 'discord_name', 'last_activity', 'total_days',
@@ -8,6 +9,7 @@ class TraineeAdmin(admin.ModelAdmin):
     list_filter = ('last_activity',)
     search_fields = ('discord_id', 'discord_name')
     readonly_fields = ('last_activity', 'total_days', 'total_problems', 'highest_streak', 'current_streak')
+
 
 @admin.register(TraineeRecord)
 class TraineeRecordAdmin(admin.ModelAdmin):
